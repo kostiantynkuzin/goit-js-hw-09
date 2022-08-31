75e4603bac15d86d2ab694f5de87cb3c247f7e10
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-31 20:19:20 +0300

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  const dataForm = new FormData(form);
+  const finalData = {};
+  for (const [key, value] of dataForm.entries()) {
+    finalData[key] = Number(value);
+  }
+  form.reset();
+
+  for (let position = 1; position <= finalData.amount; position += 1) {
+    createPromise(position, finalData.delay).then(onSuccess).catch(onError);
+    finalData.delay = finalData.delay + finalData.step;
   }
 }
+
+function onError({ position, delay }) {
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+function onSuccess({ position, delay }) {
+  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+formEl.addEventListener('submit', onSubmit);
